aa42d30a0f6c93b553fde29878fd8ebeb4c51e1a
Khalid Ahmed <csewebmail@gmail.com>
2020-09-23 11:19:55 +0600

    install and configure env pkg

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,1 +1,12 @@
 <?php
+
+if(!isset($_SESSION))
+    session_start();
+
+/**
+ * Load env file
+ */
+require_once __DIR__."/../app/Config/env.php";
+
+$app_name = getenv('APP_NAME');
+var_dump($app_name);
